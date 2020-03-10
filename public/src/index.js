1d5f75abafdadb6e1e3587c8a49b2caa5a1708d9
Magalyae <magalyae2901@gmail.com>
2020-03-10 02:50:13 -0500

    creacion proyecto para usar redux

---
+++ public/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+
+ReactDOM.render(Root, document.getElementById('root'));
